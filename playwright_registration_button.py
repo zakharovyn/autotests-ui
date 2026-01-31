47d23ba180902c627267f24da778d30e22cd42fe
Ян Захаров <banjumingray@bk.ru>
2026-01-31 13:10:34 +0700

    Углубление в Playwright. Практика по углубленной работе с Playwright

--- playwright_registration_button.py
+++ playwright_registration_button.py
@@ -5,7 +5,10 @@ with sync_playwright() as playwright:
     browser = playwright.chromium.launch(headless=False)
     page = browser.new_page()
 
-    page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration")
+    page.goto('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration')
+
+    registration_button = page.get_by_test_id('registration-page-registration-button')
+    expect(registration_button).to_be_disabled()
 
     email_input = page.get_by_test_id('registration-form-email-input').locator('input')
     email_input.fill('user.name@gmail.com')
@@ -17,8 +20,7 @@ with sync_playwright() as playwright:
     password_input.fill('password')
 
     registration_button = page.get_by_test_id('registration-page-registration-button')
-    registration_button.click()
+    expect(registration_button).to_be_enabled()  # проверяем, что перешла в состояние enabled.
 
-    dashboard_title = page.get_by_test_id('dashboard-toolbar-title-text')
-    expect(dashboard_title).to_be_visible()
-    expect(dashboard_title).to_have_text('Dashboard')
+    # если бы проверяли, что кнопка стала не задизаблена(методы идентичны, применяются по контексту)
+    # expect(registration_button).not_to_be_disabled()
