2c1cadc5d327fe473dcff4e022ffcdfd8ebb0a13
Ян Захаров <banjumingray@bk.ru>
2026-02-16 13:48:17 +0700

    Логирование взаимодействий со страницей. Практика работы с логированием

--- components/base_component.py
+++ components/base_component.py
@@ -1,6 +1,11 @@
 from typing import Pattern
 
+import allure
 from playwright.sync_api import Page, expect
+
+from tools.logger import get_logger
+
+logger = get_logger("BASE_COMPONENT")
 
 
 class BaseComponent:
@@ -9,7 +14,11 @@ class BaseComponent:
         self.page = page
 
     def check_current_url(self, expected_url: Pattern[str]):
-        expect(self.page).to_have_url(expected_url)
+        step = f'Checking that the url is equal to {expected_url}'
+
+        with allure.step(step):
+            logger.info(step)
+            expect(self.page).to_have_url(expected_url)
 
     def refresh_page(self):
         self.page.reload()
--- pages/base_page.py
+++ pages/base_page.py
@@ -3,6 +3,10 @@ from typing import Pattern
 from playwright.sync_api import Page, expect
 import allure
 
+from tools.logger import get_logger
+
+logger = get_logger("BASE_PAGE")
+
 
 class BasePage:
 
@@ -10,13 +14,22 @@ class BasePage:
         self.page = page
 
     def visit(self, url: str):
-        with allure.step(f'Opening the url "{url}"'):
+        step = f'Opening the url "{url}"'
+
+        with allure.step(step):
+            logger.info(step)
             self.page.goto(url, wait_until='networkidle')
 
     def reload(self):
-        with allure.step(f'Reloading page with url "{self.page.url}"'):
+        step = f'Reloading page with url "{self.page.url}"'
+
+        with allure.step(step):
+            logger.info(step)
             self.page.reload(wait_until='networkidle')
 
     def check_current_url(self, expected_url: Pattern[str]):
-        with allure.step(f'Checking that current url matches pattern "{expected_url.pattern}"'):
+        step = f'Checking that current url matches pattern "{expected_url.pattern}"'
+
+        with allure.step(step):
+            logger.info(step)
             expect(self.page).to_have_url(expected_url)
